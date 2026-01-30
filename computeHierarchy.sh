cd946fb115c928dd0addacb7ecd23585872aa124
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2026-01-30 16:09:38 +0100

    computeHierarchy.sh,run-all-ore.sh: Fix issues when dir names contain spaces

--- computeHierarchy.sh
+++ computeHierarchy.sh
@@ -10,18 +10,18 @@ echo Using ontology $ONTOLOGY
 
 # Step 1: extract N-triples from OWL-file
 
-java -cp bisimulation-graphs-1.0-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.bisimulations.cmd.ABox2NTriples $ONTOLOGY
+java -cp bisimulation-graphs-1.0-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.bisimulations.cmd.ABox2NTriples "$ONTOLOGY"
 
-export ONT_NAME=`basename $ONTOLOGY`
+export ONT_NAME=`basename "$ONTOLOGY"`
 
 # Step 2: run summarization
 
 cd lod_summarization/$LOD_ID/scripts
 
-./run_all.sh ../../../$ONT_NAME.nt -y
+./run_all.sh "../../../$ONT_NAME.nt" -y
 
 cd ../../..
 
 # Step 3: run conceptual clustering
 
-java -cp bisimulation-graphs-1.0-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.bisimulations.cmd.ExtractHierarchy lod_summarization/$LOD_ID/$ONT_NAME/ $ONTOLOGY $MAX_DEPTH $MAX_ITERATIONS
+java -cp bisimulation-graphs-1.0-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.bisimulations.cmd.ExtractHierarchy "lod_summarization/$LOD_ID/$ONT_NAME/" "$ONTOLOGY" $MAX_DEPTH $MAX_ITERATIONS
--- run-all-ore.sh
+++ run-all-ore.sh
@@ -2,12 +2,12 @@
 
 export ORE_PATH=./ore2015_sample/pool_sample/
 
-for ont in `cat $ORE_PATH/el/instantiation/fileorder.txt`
+for ont in `cat "$ORE_PATH/el/instantiation/fileorder.txt" | sed 's/\r//'`
 do
 
     echo $ont
 
-    /usr/bin/time -f "TOTAL TIME: %e" timeout 600 ./computeHierarchy.sh $ORE_PATH/files/$ont 2 10 &> $ont.log
-    mv output.owl $ont.simGraph.owl
-    mv clustering-result.owl $ont.clustering.owl
+    /usr/bin/time -f "TOTAL TIME: %e" timeout 600 ./computeHierarchy.sh "$ORE_PATH/files/$ont" 2 10 &> $ont.log
+    mv output.owl "$ont.simGraph.owl"
+    mv clustering-result.owl "$ont.clustering.owl"
 done
